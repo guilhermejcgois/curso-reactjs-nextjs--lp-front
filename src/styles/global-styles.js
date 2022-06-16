a03f1c24ce804dbbb209857266a4ebb88dfd151e
Guilherme Gois <guijocargo@gmail.com>
2022-06-16 17:15:23 -0300

    chore: configure styled-components

---
+++ src/styles/global-styles.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+            sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+
+        ${({ theme }) => css`
+          background: ${theme.colors.mainBg};
+        `}
+    }
+
+    code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+        monospace;
+    }
+`;
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,66 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import './styles.css';
-
-import { Posts } from '../../components/Posts';
-import { loadPosts } from '../../utils/load-posts';
-import { Button } from '../../components/Button';
-import { TextInput } from '../../components/TextInput';
+import * as Styled from './styles';
 
 export const Home = () => {
-  const [allPosts, setAllPosts] = useState([]);
-  const [filteredPosts, setFilteredPosts] = useState([]);
-  const [hasMorePosts, setHasMorePosts] = useState(false);
-  const [page, setPage] = useState(0);
-  const [posts, setPosts] = useState([]);
-  const [postsPerPage] = useState(2);
-  const [searchValue, setSearchValue] = useState('');
-
-  const loadMorePosts = () => {
-    const nextPage = page + postsPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-
-    setPosts(posts.concat(nextPosts));
-    setPage(nextPage);
-  };
-
-  const updateSearchValue = (event) => setSearchValue(event.target.value);
-
-  useEffect(() => {
-    const loadData = async () => {
-      const loadedPosts = await loadPosts();
-      setAllPosts(loadedPosts);
-      setPosts(loadedPosts.slice(page, postsPerPage));
-    };
-
-    loadData();
-  }, []);
-
-  useEffect(() => {
-    setHasMorePosts(page + postsPerPage < allPosts.length);
-  }, [page, postsPerPage, allPosts]);
-
-  useEffect(() => {
-    const matchesTitle = ({ title }) => title && title.toLowerCase().includes(searchValue.toLowerCase());
-    const filteredPosts = searchValue ? allPosts.filter(matchesTitle) : posts;
-    setFilteredPosts(filteredPosts);
-  }, [searchValue, allPosts, posts]);
-
   return (
-    <section className="container">
-      <div className="search-container">
-        {!!searchValue && <h1>Search value: {searchValue}</h1>}
-
-        <TextInput searchValue={searchValue} handleChange={updateSearchValue} />
-      </div>
-
-      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
-
-      {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
-
-      <div className="button-container">
-        {!searchValue && <Button text="Load more posts" onClick={loadMorePosts} disabled={!hasMorePosts} />}
-      </div>
-    </section>
+    <div className="App">
+      <Styled.Wrapper background="blue">
+        <h1>Hello</h1>
+      </Styled.Wrapper>
+    </div>
   );
 };
