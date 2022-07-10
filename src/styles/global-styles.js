f05e4785f0b288a7c61a6be22f33b6cafbe30f5b
Guilherme Gois <guijocargo@gmail.com>
2022-07-10 11:15:36 -0300

    feat: update font and theme

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,27 +1,24 @@
-import { createGlobalStyle, css } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
     }
 
-    body {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-            sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-
-        ${({ theme }) => css`
-          background: ${theme.colors.mainBg};
-        `}
+    html {
+      font-size: 62.5%;
     }
 
-    code {
-        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-        monospace;
+    body {
+      font-size: 1.6rem;
+      font-family: ${({ theme }) => theme.font.family.default}
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+      font-family: ${({ theme }) => theme.font.family.secondary}
     }
 `;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,41 @@
 export const theme = {
   colors: {
-    mainBg: 'red',
-    secondaryBg: 'blue',
-    primaryText: 'black',
+    mainBg: '#0a1128',
+    secondaryBg: '#dc143c',
+    light: '#ffffff',
+    lightBg: '#ffffff',
+    darkBg: '#0a1128',
+    primaryColor: '#0a1128',
+    secondaryColor: '#dc143c',
+    white: `#ffffff`,
   },
-  fonts: {},
-  spacing: {},
+  font: {
+    family: {
+      default: "'Open Sans', sans-serif;",
+      secondary: "'Montserrat', sans-serif;",
+    },
+    sizes: {
+      xsmall: '0.8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+  },
+  spacing: {
+    xsmall: '0.8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
 };
